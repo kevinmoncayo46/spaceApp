ce498562f8c94306f230b3f6492052cfe5ad00c0
Kevin Moncayo <kevinmoncayo46@gmail.com>
2024-07-09 14:34:05 -0500

    primer commit

---
+++ ejemplos/callback.js
@@ -0,0 +1,33 @@
+function uno (dos) {
+    console.log("uno")
+    setTimeout(dos,2000)
+    
+}
+
+function dos (tres) {
+    console.log("dos")
+    setTimeout(tres,2000)
+    
+}
+
+function tres (cuatro) {
+    console.log("tres")
+    setTimeout(cuatro,2000)
+    
+}
+
+function cuatro (cinco) {
+    console.log("cuatro")
+    setTimeout(cinco,2000)
+    
+}
+
+function cinco() {
+    console.log("cinco")
+}
+
+setTimeout(() => uno(dos),5000)
+
+
+//callback hell
+
---
+++ script.js
@@ -0,0 +1,46 @@
+
+const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15"
+
+async function listaImagenes(){
+    try{
+        let fetchImagen = await fetch(url)
+        let datosImagenes = await fetchImagen.json()
+
+        const card = document.querySelector("[data-ul]")
+
+        datosImagenes.forEach( elemento => {
+            const contenido = `<li class="card">
+                    <img class="card__image" src="${elemento.url}" alt="imagen">
+                    <h3 class="card__title">${elemento.title}</h3>
+                </li>
+            `
+            card.innerHTML = card.innerHTML + contenido
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+
+// function listaImagenes(){
+//     fetch(url)
+//     .then( response => response.json())
+//     .then( datosImagenes => {
+//         console.log(datosImagenes)
+
+//         const card = document.querySelector("[data-ul]")
+
+//         datosImagenes.forEach( elemento => {
+//             const contenido = `<li class="card">
+//                     <img class="card__image" src="${elemento.url}" alt="imagen">
+//                     <h3 class="card__title">${elemento.title}</h3>
+//                 </li>
+//             `
+//             card.innerHTML = card.innerHTML + contenido
+//         })
+//     })
+//     .catch( error => console.log(error) )
+// }
+
+listaImagenes()
